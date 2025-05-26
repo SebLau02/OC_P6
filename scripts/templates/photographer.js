e180654341f21c6ef9dbece770eb4bcbd6f44bc8
Sébastien Lau <sebastien.lau2021@gmail.com>
2025-05-26 14:54:51 +0200

    redesign index html

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,39 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, price, country, tagline } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const link = document.createElement("a");
+    link.setAttribute("href", "photographer.html");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.loading = "lazy";
+    img.alt = name;
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+
+    const localisation = document.createElement("p");
+    localisation.innerText = `${city}, ${country}`;
+
+    const description = document.createElement("p");
+    description.innerText = tagline;
+
+    const tjm = document.createElement("p");
+    tjm.innerText = `${price}€/jour`;
+
+    const p = document.createElement("div");
+    p.appendChild(localisation);
+    p.appendChild(description);
+    p.appendChild(tjm);
+
+    link.appendChild(img);
+    link.appendChild(h2);
+    article.appendChild(link);
+    article.appendChild(p);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }

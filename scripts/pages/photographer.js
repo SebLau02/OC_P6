64847c739f0f0b7f40f4ea3af5048c7cc3b933f8
Sébastien Lau <sebastien.lau2021@gmail.com>
2025-05-26 21:37:42 +0200

    add medias gallery

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,3 +1,5 @@
+const cardContainer = document.querySelector("#card-container");
+
 const getPhotographer = async () => {
   try {
     const res = await fetch("data/photographers.json");
@@ -10,8 +12,19 @@ const getPhotographer = async () => {
     return error;
   }
 };
-
-const displayData = (photographer) => {
+const getPhotos = async () => {
+  try {
+    const res = await fetch("data/medias.json");
+    const data = await res.json();
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+    const medias = data.filter((m) => m.photographerId === parseInt(id));
+    return { medias };
+  } catch (error) {
+    return error;
+  }
+};
+const displayProfilData = (photographer) => {
   const { name, city, country, tagline, portrait } = photographer;
 
   const photographerContainer = document.querySelector(".photograph-header");
@@ -31,9 +44,56 @@ const displayData = (photographer) => {
   pictureBox.src = `assets/photographers/${portrait}`;
 };
 
+const createMediaCard = (media) => {
+  if ("video" in media) {
+    console.log(media);
+  }
+  const article = document.createElement("article");
+  let mediaElement = "";
+  if ("image" in media) {
+    mediaElement = document.createElement("img");
+    mediaElement.src = `../../assets/medias/${media.image}`;
+    mediaElement.alt = media.title;
+  }
+  if ("video" in media) {
+    const source = document.createElement("source");
+    source.src = `../../assets/medias/${media.video}`;
+    source.type = "video/mp4";
+    mediaElement = document.createElement("video");
+    mediaElement.appendChild(source);
+    mediaElement.setAttribute("controls", "true");
+  }
+  const div = document.createElement("div");
+  const titleBox = document.createElement("span");
+  const likesBox = document.createElement("span");
+  const likesIcon = document.createElement("img");
+
+  titleBox.innerText = media.title;
+  likesBox.innerText = media.likes;
+  likesIcon.src = "../../assets/icons/heart.svg";
+  likesIcon.alt = "icône coeur";
+
+  likesBox.appendChild(likesIcon);
+  div.appendChild(titleBox);
+  div.appendChild(likesBox);
+
+  article.appendChild(mediaElement);
+  article.appendChild(div);
+
+  cardContainer.appendChild(article);
+};
+
+const displayMedias = (medias) => {
+  medias.forEach((media) => {
+    createMediaCard(media);
+  });
+};
+
 const init = async () => {
   const { photographer } = await getPhotographer();
-  displayData(photographer);
+  const { medias } = await getPhotos();
+  displayProfilData(photographer);
+  displayMedias(medias);
 };
 
 init();

0920ca9afe5404282cefa0fe326f1bbaa513e3e7
Sébastien Lau <sebastien.lau2021@gmail.com>
2025-06-02 18:40:48 +0200

    add aria to contact modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -3,6 +3,8 @@ function displayModal() {
   const html = document.querySelector("html");
   modal.classList.add("active");
   html.classList.add("hidden");
+  const dialog = modal.querySelector(".modal");
+  dialog.focus();
 }
 
 function closeModal() {
